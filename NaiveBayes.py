9439c23c2f6dab5f0b29f204dcd20c07a69d94b4
Lillith Chute <LillithChute@maine.rr.com>
2022-11-18 14:30:28 -0500

    Added heatmap and removed categories
    I removed two of the categories that had very low accuracy and F1 scores from the 13 categories I was sampling.  I also generated a heatmap to see which categories were being misclassified frequently.

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -6,20 +6,30 @@
 #                                                                                               #
 #################################################################################################
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 
 # Reading the data from pickle file where it has already been processed
 injuryDataFrame = pd.read_pickle("Data/injury_descriptions_processed.pkl")
 
+# Removed troublesome category here.  For some reason this one is a very low predictor.
+injuryDataFrame = injuryDataFrame[
+    ~injuryDataFrame.InjuryCauseDesc.str.contains("Bodily reaction and exertion, unspecified")]
+injuryDataFrame = injuryDataFrame[~injuryDataFrame.InjuryCauseDesc.str.contains("Bodily reaction, n.e.c.")]
+
+# labels for heat map/confusion matrix
+
+category_id_df = injuryDataFrame[['InjuryCauseDesc']].drop_duplicates().sort_values('InjuryCauseDesc')
 # Now that the data has been processed, split it.
 X_train, X_test, y_train, y_test = train_test_split(injuryDataFrame['processed_desc'],
                                                     injuryDataFrame['InjuryCauseDesc'],
                                                     random_state=0)
 
-vectorizer = TfidfVectorizer(stop_words="english", max_features=1000, decode_error="ignore", ngram_range=(1, 3))
+vectorizer = TfidfVectorizer(stop_words="english", max_features=10000, decode_error="ignore", ngram_range=(1, 3))
 vectorizer.fit(X_train)
 
 cls = MultinomialNB()
@@ -28,5 +38,16 @@ cls = MultinomialNB()
 cls.fit(vectorizer.transform(X_train), y_train)
 
 y_pred = cls.predict(vectorizer.transform(X_test))
+
+# Display metrics
 print(accuracy_score(y_test, y_pred))
 print(classification_report(y_test, y_pred))
+
+# Display heatmap confusion matrix
+conf_mat = confusion_matrix(y_test, y_pred)
+fig, ax = plt.subplots(figsize=(15, 13))
+sns.heatmap(conf_mat, annot=True, fmt='d',
+            xticklabels=category_id_df.InjuryCauseDesc.values, yticklabels=category_id_df.InjuryCauseDesc.values)
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.show()
