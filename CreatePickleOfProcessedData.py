d210fe79d5a4e45cc07beae1f9c04998531c1ccd
Lillith Chute <LillithChute@maine.rr.com>
2022-11-18 13:26:15 -0500

    Update to loading data
    I created a file that can be run that will do the data pre-processing and turn that file into a pickle file.  This is because the lemmatizing process take a very long time.  Once the pkl file is created, we can play around with the creation of algorithms and train from the already preprocessed data.

---
+++ CreatePickleOfProcessedData.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from NaiveBayesTextProcessing import remove_null_rows_from_injury_dataset
+from TextPreprocessing import lemmatize_sentence, normalize
+
+# Reading the data
+injuryDataFrame = pd.read_csv("Data/InjuryCauseTopThirteen.csv", sep=',', encoding='latin-1',
+                              usecols=lambda col: col not in ["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"])
+
+# Quick exam of the data
+print(injuryDataFrame.head())
+
+# What does the text look like?
+print("\n\nSome example injury text:")
+print(injuryDataFrame["InjuryDesc"].to_list()[:5])
+
+# Remove the rows that have null
+injuryDataFrame = remove_null_rows_from_injury_dataset(injuryDataFrame)
+
+# Normalize the data
+injuryDataFrame['processed_desc'] = [normalize(x) for x in injuryDataFrame['InjuryDesc']]
+
+# What does the text look like now?
+print("\n\nSome example injury text having been normalized:")
+print(injuryDataFrame["processed_desc"].to_list()[:5])
+
+# Lemmatize the sentence.  This takes a while to run on a standard machine
+injuryDataFrame['processed_desc'] = [lemmatize_sentence(x) for x in injuryDataFrame['processed_desc']]
+
+# What does the text look like now?
+print("\n\nSome example injury text having been lemmatized:")
+print(injuryDataFrame["processed_desc"].to_list()[:5])
+
+injuryDataFrame.to_pickle("Data/injury_descriptions_processed.pkl")
--- NaiveBayes.py
+++ NaiveBayes.py
@@ -11,43 +11,15 @@ from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 
-from NaiveBayesTextProcessing import remove_null_rows_from_injury_dataset
-from TextPreprocessing import normalize, lemmatize_sentence
-
-# Reading the data
-injuryDataFrame = pd.read_csv("Data/InjuryCauseTopThirteen.csv", sep=',', encoding='latin-1',
-                              usecols=lambda col: col not in ["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"])
-
-# Quick exam of the data
-print(injuryDataFrame.head())
-
-# What does the text look like?
-print("\n\nSome example injury text:")
-print(injuryDataFrame["InjuryDesc"].to_list()[:5])
-
-# Remove the rows that have null
-injuryDataFrame = remove_null_rows_from_injury_dataset(injuryDataFrame)
-
-# Normalize the data
-injuryDataFrame['processed_desc'] = [normalize(x) for x in injuryDataFrame['InjuryDesc']]
-
-# What does the text look like now?
-print("\n\nSome example injury text having been normalized:")
-print(injuryDataFrame["processed_desc"].to_list()[:5])
-
-# Lemmatize the sentence.  This takes a while to run on a standard machine
-injuryDataFrame['processed_desc'] = [lemmatize_sentence(x) for x in injuryDataFrame['processed_desc']]
-
-# What does the text look like now?
-print("\n\nSome example injury text having been lemmatized:")
-print(injuryDataFrame["processed_desc"].to_list()[:5])
+# Reading the data from pickle file where it has already been processed
+injuryDataFrame = pd.read_pickle("Data/injury_descriptions_processed.pkl")
 
 # Now that the data has been processed, split it.
 X_train, X_test, y_train, y_test = train_test_split(injuryDataFrame['processed_desc'],
                                                     injuryDataFrame['InjuryCauseDesc'],
                                                     random_state=0)
 
-vectorizer = TfidfVectorizer(stop_words="english", max_features=1000, decode_error="ignore", ngram_range=(2, 3))
+vectorizer = TfidfVectorizer(stop_words="english", max_features=1000, decode_error="ignore", ngram_range=(1, 3))
 vectorizer.fit(X_train)
 
 cls = MultinomialNB()
